df736c12b6280781d75431b8cc2ee730d40a1c6e
jeanfrancoM <jeanfranco.mesias.palomino@gmail.com>
2026-02-06 15:53:12 -0500

    avanzando de franco

--- MilagrosFuent/capadatos/BancosCD.cs
+++ MilagrosFuent/capadatos/BancosCD.cs
@@ -74,6 +74,37 @@ namespace CapaDatos
           finally { dta_consulta.Dispose(); }
       }
 
+      //avanzdo de franco 
+      public DataTable F_Listar_Bancos()
+      {
+          DataTable dta_consulta = null;
+
+          try
+          {
+              using (SqlConnection sql_conexion = new SqlConnection())
+              {
+                  sql_conexion.ConnectionString = ConfigurationManager.ConnectionStrings["BDCONEXION"].ConnectionString;
+                  sql_conexion.Open();
+
+                  using (SqlCommand sql_comando = new SqlCommand())
+                  {
+                      sql_comando.Connection = sql_conexion;
+                      sql_comando.CommandType = CommandType.StoredProcedure;
+                      sql_comando.CommandText = "pa_Bancos_Listar";
+
+                      dta_consulta = new DataTable();
+                      dta_consulta.Load(sql_comando.ExecuteReader());
+                      return dta_consulta;
+                  }
+              }
+          }
+          catch (Exception ex)
+          {
+              throw ex;
+          }
+          finally { dta_consulta.Dispose(); }
+      }
+
 
       public DataTable F_Listar_NroCuenta(BancosCE objEntidadBE)
       {
@@ -123,7 +154,7 @@ namespace CapaDatos
           finally { dta_consulta.Dispose(); }
 
       }
-      //enzo metio mano aqui 
+
       public DataTable F_Listar_Bancos_Milagros()
       {
           DataTable dta_consulta = null;
@@ -155,4 +186,3 @@ namespace CapaDatos
       }
     }
 }
-//franquito metio mano aca y con segunda prueva
